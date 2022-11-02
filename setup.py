33046c2841121583b525a929d18e03b0196e7cb3
Viswanatha Krishnamurthy <viswanatha.krishnamurthy@kyndryl.com>
2022-11-02 22:30:36 +0530

    Files for the project

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+
+def get_requirments():
+    requirements_list = []
+    return 
+
+setup(
+    name="sensor",
+    version='0.0.1',
+    author='Viswanatha Krishnamurthy',
+    author_email='vishwa83@gmail.com',
+    packages=find_packages(),
+    install_requires = get_requirments()
+)
+
+for i in sensor-fault-detection.requirements.txt:
+    print(i)
